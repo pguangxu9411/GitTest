4c7c590634ff73665aa849aa062abc3dcdb234d1
piao <405551007@qq.com>
2016-12-28 15:57:12 +0800

    Rejected commit: Default

--- src/World.java
+++ src/World.java
@@ -2,6 +2,34 @@
  * Created by Mr.piao on 2016/12/28.
  */
 public class World {
+    private String name;
+    private Double age;
+    private boolean xb;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getAge() {
+        return age;
+    }
+
+    public boolean isXb() {
+        return xb;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Double age) {
+        this.age = age;
+    }
+
+    public void setXb(boolean xb) {
+        this.xb = xb;
+    }
+
     public static void main(String[] args) {
         int a=11;
         System.out.println(a);
